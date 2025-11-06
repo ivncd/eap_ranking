5d26707d1962591068f8dd976ae73b894402b4b3
ivan <ivancoto03@gmail.com>
2025-11-06 15:21:29 +0100

    Primer commit

---
+++ ranking.py
@@ -0,0 +1,61 @@
+import requests
+from collections import defaultdict
+from bs4 import BeautifulSoup
+from tabulate import tabulate
+
+URL = "http://138.100.11.198/notas"
+def obtain_text():
+    r = requests.get(URL)
+    return r.text
+
+def obtain_data(text : str):
+    result = defaultdict(list)
+    soup = BeautifulSoup(text, "lxml")
+    tr = soup.find_all("tr")
+    for row in tr[1:]:
+        mat, user, problem_id, contest_id, level, grade = [col.getText().strip() for col in row.children if col.getText().strip() != ""]
+        contest_id = int(contest_id)
+        problem_id = int(problem_id)
+        grade = float(grade)
+
+        if contest_id == 11:
+            continue
+
+        result[user].append((mat, user, problem_id, contest_id, level, grade))
+
+    return result
+
+def obtain_grades(result):
+    grades = {}
+    for user, values in result.items():
+        total = defaultdict(int)
+        for value in values:
+            level, grade = value[4], value[5]
+            total[level] += grade
+
+        max_a = min(5, total["A"])
+        max_b = min(7, max_a + total["B"])
+        max_c = min(9, max_b + total["C"])
+        max_d = min(10, max_c + total["D"])
+
+        grades[user] = (max_a, max_b, max_c, max_d)
+
+    return grades.items()
+
+def get_table(grades_list):
+    table = [[pos+1, user, *grades] for pos, (user, grades) in enumerate(grades_list)]
+    return tabulate(table, headers=["Ranking", "Usuario", "Nota A /5", "Nota A+B /7", "Nota A+B+C /9", "Nota A+B+C+D /10"],tablefmt="github")
+
+
+def start():
+    text = obtain_text()
+    result = obtain_data(text)
+    grades = sorted(obtain_grades(result), key=lambda x: -x[1][-1])
+
+    table = get_table(grades)
+
+    with open("index.md", "w+") as f:
+        f.write(table)
+
+if __name__ == "__main__":
+    start()
